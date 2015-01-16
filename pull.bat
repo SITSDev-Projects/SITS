df02b11ee2e2575101c941b9ef28f0e3268621d1
DarrenDexter <Darren.Dexter@4PD0J12-LT-HU.tribalgroup.net>
2015-01-16 13:55:37 +0000

    SITS

--- pull.bat
+++ pull.bat
@@ -1,3 +1,3 @@
 S:
-cd S:\Documents\Product Development\Teams\TM2 - Geek Clique (Interfaces)\Documentation\GitHub\SitsDev
+cd S:\Documents\Product Development\Teams\TM2 - Geek Clique (Interfaces)\Documentation\GitHub
 "C:\Program Files (x86)\Git\bin\git" pull
