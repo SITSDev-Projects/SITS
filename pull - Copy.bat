effe36f0711d445e3105af08c92e3643dd005ae7
DarrenDexter <Darren.Dexter@4PD0J12-LT-HU.tribalgroup.net>
2015-01-16 15:01:25 +0000

    SITS

--- pull - Copy.bat
+++
@@ -1,7 +0,0 @@
-S:
-cd S:\Documents\Product Development\Teams\TM2 - Geek Clique (Interfaces)\Documentation\GitHub\Test
-"C:\Program Files (x86)\Git\bin\git" init
-"C:\Program Files (x86)\Git\bin\git" remote add origin https://github.com/DarrenDexter/SITSDev-Projects.git
-"C:\Program Files (x86)\Git\bin\git" checkout master
-"C:\Program Files (x86)\Git\bin\git" pull
-pause
